bc48907a46c0c8a59c59775d2abfefce1719c7d1
okFox <smayfield@gmail.com>
2020-03-15 21:15:32 -0700

    more launch params

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer');
 
 describe('My first puppeteer test', () => {
-  it('should launch the broswer', async function() {
-    const browser = puppeteer.launch({ headless: false });
+  it('should launch the browser', async function() {
+    const browser = puppeteer.launch({ 
+      headless: false, 
+      slowMo: 50, 
+      devtools: true });
+
     const page = await (await browser).newPage();
     await page.goto('http://www.example.com/');
     await page.waitForSelector('h1');
